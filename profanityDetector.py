1517be0d7736b7ff75f6525666a1445239fc2d9b
Shivam Tyagi <shivamtyagi837@gmail.com>
2019-01-27 05:05:06 +0530

    Create profanityDetector.py

---
+++ profanityDetector.py
@@ -0,0 +1,17 @@
+import urllib.request
+
+def takeInput() :
+    user_input = input("Enter the Text : ")
+    type(user_input)
+    profanityCheck(user_input)
+    
+def profanityCheck(user_input) :
+    res = urllib.request.urlopen("https://www.purgomalum.com/service/containsprofanity?text="+user_input)
+    html = res.read()
+    
+    if b'true' in html :
+        print("PROFANITY ALERT !!! ")
+    elif b'false' in html :
+        print("NO CURSE WORD FOUND.")
+   
+takeInput()
