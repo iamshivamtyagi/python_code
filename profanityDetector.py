a9e814021aeae55d29af6492343a93c0d17c503b
Shivam Tyagi <shivamtyagi837@gmail.com>
2019-06-27 23:01:29 +0530

    Update profanityDetector.py

--- profanityDetector.py
+++ profanityDetector.py
@@ -1,12 +1,12 @@
 import urllib.request
 
 def takeInput() :
-    user_input = input("Enter the Text : ")
+    user_input = input(" Enter the Text : ")
     type(user_input)
     profanityCheck(user_input)
     
 def profanityCheck(user_input) :
-    res = urllib.request.urlopen("https://www.purgomalum.com/service/containsprofanity?text="+user_input)
+    res = urllib.request.urlopen(" https://www.purgomalum.com/service/containsprofanity?text= " + user_input)
     html = res.read()
     
     if b'true' in html :
